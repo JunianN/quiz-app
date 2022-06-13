479de373cc64d3856fb39bf1999885f72889105f
JunianN <79140318+JunianN@users.noreply.github.com>
2022-06-13 17:25:20 +0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,106 @@
+const quizData = [{
+        question: "When does Le Sserafim debut?",
+        a: "1 May 2022",
+        b: "2 May 2022",
+        c: "29 October 2022",
+        d: "29 March 2022",
+        correct: "b",
+    },
+    {
+        question: "When did Kim Chaewon born?",
+        a: "1 May 2000",
+        b: "2 May 2000",
+        c: "1 August 2000",
+        d: "29 March 2000",
+        correct: "c",
+    },
+    {
+        question: "When did IZ*ONE disband?",
+        a: "1 May 2021",
+        b: "2 May 2021",
+        c: "29 October 2021",
+        d: "29 March 2021",
+        correct: "d",
+    },
+    {
+        question: "Who is the leader of Le Sserafim?",
+        a: "Huh Yunjin",
+        b: "Kim Chaewon",
+        c: "Miyawaki Sakura",
+        d: "Kim Garam",
+        correct: "b",
+    },
+    {
+        question: "Who is the maknae of Le Sserafim?",
+        a: "Huh Yunjin",
+        b: "Kazuha",
+        c: "Hong Eunchae",
+        d: "Kim Garam",
+        correct: "c",
+    }
+]
+
+const quiz = document.getElementById('quiz');
+const answerEls = document.querySelectorAll('.answer');
+const questionEl = document.getElementById('question');
+const a_text = document.getElementById('a_text');
+const b_text = document.getElementById('b_text');
+const c_text = document.getElementById('c_text');
+const d_text = document.getElementById('d_text');
+const submitBtn = document.getElementById('submit');
+
+let currentQuiz = 0;
+let score = 0;
+
+loadQuiz();
+
+function loadQuiz() {
+    deselectAnswers();
+
+    const currentQuizData = quizData[currentQuiz];
+
+    questionEl.innerText = currentQuizData.question;
+    a_text.innerText = currentQuizData.a;
+    b_text.innerText = currentQuizData.b;
+    c_text.innerText = currentQuizData.c;
+    d_text.innerText = currentQuizData.d;
+}
+
+function getSelected() {
+    let answer = undefined;
+
+    answerEls.forEach((answerEl) => {
+        if (answerEl.checked) {
+            answer = answerEl.id;
+        }
+    })
+
+    return answer;
+}
+
+function deselectAnswers() {
+    answerEls.forEach((answerEl) => {
+        answerEl.checked = false;
+    });
+}
+
+submitBtn.addEventListener('click', () => {
+    const answer = getSelected();
+
+    if (answer) {
+        if (answer === quizData[currentQuiz].correct) {
+            score++;
+        }
+
+        currentQuiz++;
+        if (currentQuiz < quizData.length) {
+            loadQuiz();
+        } else {
+            quiz.innerHTML = `
+            <h2>Score : ${score/quizData.length*100}%.</h2>
+    
+            <button onclick="location.reload()">Try Again</button>
+            `
+        }
+    }
+});
